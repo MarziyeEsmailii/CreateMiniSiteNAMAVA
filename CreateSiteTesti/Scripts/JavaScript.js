f1d001ffec0734a5352a3d79230798937798e557
Marziue esmaili <marziyeitmarziye@gmail.com>
2024-05-06 20:36:02 +0330

    Mini site namava with navigation and simple main and footer

---
+++ CreateSiteTesti/Scripts/JavaScript.js
@@ -0,0 +1,10 @@
+﻿
+function updateDateTime() {
+    const now = new Date();
+    const dateTimeString = now.toLocaleString();
+    document.getElementById('date-time').textContent = dateTimeString;
+}
+
+// Update the date and time immediately and then every second
+updateDateTime();
+setInterval(updateDateTime, 1000);
